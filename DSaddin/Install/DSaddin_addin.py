4d1c0c65a2ae1abf7e1e7838c2124577e15c6ea2
Nathan Seese <seese.28@osu.edu>
2016-04-24 14:42:11 -0400

    Add-in and toolbox updates

--- DSaddin/Install/DSaddin_addin.py
+++ DSaddin/Install/DSaddin_addin.py
@@ -1,18 +1,20 @@
 import arcpy
 import pythonaddins
-import result_dialog
-
 
 class DistrictButon(object):
     """Implementation for DSaddin_district.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
-
     def onClick(self):
-        app = RedistrictingResults()
-        app.mainloop()
-        # TODO definitley not this....
-        pythonaddins.GPToolDialog(("c:/Users/n44635/Documents/"
-                                   "ArcGIS/Redistricting.pyt"),
+        pythonaddins.GPToolDialog("c:/Users/n44635/Documents/ArcGIS/Redistricting.pyt",
                                   "Add_Integer_Field_tool")
+        
+class BuildDistrict(object):
+    """Implementation for DSaddin.build_layers (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pythonaddins.GPToolDialog("c:/Users/n44635/Documents/ArcGIS/Redistricting.pyt",
+                                  "Build_Districts_tool")
