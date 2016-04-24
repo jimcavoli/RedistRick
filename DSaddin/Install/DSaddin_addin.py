5d1ed0ac1d3ed532234a2c412c00dfaaa1e0b909
Jim Cavoli <jimcavoli@users.noreply.github.com>
2016-04-24 13:00:44 -0400

    Style cleanup

--- DSaddin/Install/DSaddin_addin.py
+++ DSaddin/Install/DSaddin_addin.py
@@ -1,11 +1,15 @@
 import arcpy
 import pythonaddins
 
+
 class DistrictButon(object):
     """Implementation for DSaddin_district.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
+
     def onClick(self):
-        pythonaddins.GPToolDialog("c:/Users/n44635/Documents/ArcGIS/Redistricting.pyt",
+        # TODO definitley not this....
+        pythonaddins.GPToolDialog(("c:/Users/n44635/Documents/"
+                                   "ArcGIS/Redistricting.pyt"),
                                   "Add_Integer_Field_tool")
--- DSaddin/makeaddin.py
+++ DSaddin/makeaddin.py
@@ -4,10 +4,11 @@ import zipfile
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 
-out_zip_name = os.path.join(current_path, 
+out_zip_name = os.path.join(current_path,
                             os.path.basename(current_path) + ".esriaddin")
 
 BACKUP_FILE_PATTERN = re.compile(".*_addin_[0-9]+[.]py$", re.IGNORECASE)
+
 
 def looks_like_a_backup(filename):
     return bool(BACKUP_FILE_PATTERN.match(filename))
