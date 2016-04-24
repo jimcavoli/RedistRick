27b120fdda86238628fa09f5b3347a1429d1b5e3
Jim Cavoli <jimcavoli@users.noreply.github.com>
2016-04-24 11:02:17 -0400

    Continuing to address CC quality issues

--- Redistricting.py
+++ Redistricting.py
@@ -3,9 +3,10 @@
 # The Ohio State University
 # Geog5223: GIS Design and Implementation
 
+import os
+import sys
 import arcpy
 from arcpy import env
-import os, sys
 
 
 # Custom Functions:
@@ -18,7 +19,7 @@ import os, sys
 # Districts are numbered 1 to n
 def numDistricts(infc):
         field_values = []
-        rows = arcpy.SearchCursor(infc,fields = "Dist_ID")
+        rows = arcpy.SearchCursor(infc, fields="Dist_ID")
         for row in rows:
             instance = row.getValue("Dist_ID")
             field_values.append(instance)
@@ -51,30 +52,30 @@ class Add_Integer_Field_Tool(object):
         """Define parameter definitions"""
         # input TIGER file layer (shp file)
         param0 = arcpy.Parameter(
-            displayName = "Input Feature Class",
-            name = "in_feature",
-            datatype = "DEFeatureClass",
-            parameterType = "Required",
-            direction = "Input")
+            displayName="Input Feature Class",
+            name="in_feature",
+            datatype="DEFeatureClass",
+            parameterType="Required",
+            direction="Input")
 
         # Name of Field to be Added
         param1 = arcpy.Parameter(
-            displayName = "New Field",
-            name = "new_field",
-            datatype = "GPString",
-            parameterType = "Required",
-            direction = "Input")
+            displayName="New Field",
+            name="new_field",
+            datatype="GPString",
+            parameterType="Required",
+            direction="Input")
 
         # Value to populate the field
         param2 = arcpy.Parameter(
-            displayName = "Field Integer Value",
-            name = "field_value",
-            datatype = "GPLong",
-            parameterType = "Required",
-            direction = "Input")
+            displayName="Field Integer Value",
+            name="field_value",
+            datatype="GPLong",
+            parameterType="Required",
+            direction="Input")
 
         # list of parameters for tool
-        parameters = [param0,param1,param2]
+        parameters = [param0, param1, param2]
         return parameters
 
     def isLicensed(self):
@@ -90,14 +91,13 @@ class Add_Integer_Field_Tool(object):
         # -----------------------------------------------------------------
         # Frequently Used Variables
         code_directory = 'E:\\redistricting_data\code'
-        infc_directory = os.path.split(arcpy.Describe(infc).catalogPath)[0]
+        infc_dir = os.path.split(arcpy.Describe(infc).catalogPath)[0]
         infc_name = arcpy.Describe(infc).file
         write_file_name = 'temp_output.txt'
         # -----------------------------------------------------------------
 
-
         # define the workspace using the path name of the input feature class
-        env.workspace = infc_directory
+        env.workspace = infc_dir
 
         # Add Field to the Feature Class
         try:
@@ -123,35 +123,35 @@ class Split_Layer_Tool(object):
         """Define parameter definitions"""
         # Input Feature Class
         param0 = arcpy.Parameter(
-            displayName = "Input Feature Class",
-            name = "in_feature",
-            datatype = "DEFeatureClass",
-            parameterType = "Required",
-            direction = "Input")
+            displayName="Input Feature Class",
+            name="in_feature",
+            datatype="DEFeatureClass",
+            parameterType="Required",
+            direction="Input")
 
         # only accept Polygon Feature Class
         param0.filter.list = ["Polygon"]
 
         # Field Selection is Based off of
         param1 = arcpy.Parameter(
-            displayName = "Field Name",
-            name = "field_name",
-            datatype = "Field",
-            parameterType = "Required",
-            direction = "Input")
+            displayName="Field Name",
+            name="field_name",
+            datatype="Field",
+            parameterType="Required",
+            direction="Input")
         param1.parameterDependencies = [param0.name]
         param1.filter.list = ["LONG", "SHORT"]
 
         # Outpath
         param2 = arcpy.Parameter(
-            displayName = "Output Folder",
-            name = "field_value",
-            datatype = "GPFeatureLayer",
-            parameterType = "Required",
-            direction = "Output")
+            displayName="Output Folder",
+            name="field_value",
+            datatype="GPFeatureLayer",
+            parameterType="Required",
+            direction="Output")
 
         # list of parameters for tool
-        parameters = [param0,param1,param2]
+        parameters = [param0, param1, param2]
         return parameters
 
     def isLicensed(self):
@@ -162,13 +162,13 @@ class Split_Layer_Tool(object):
 
         infc = parameters[0].valueAsText
         field = parameters[1].valueAsText
-        infc_directory = arcpy.Describe(infc).catalogPath
+        infc_dir = arcpy.Describe(infc).catalogPath
         infc_name = arcpy.Describe(infc).file
 
-        env.workspace = infc_directory
+        env.workspace = infc_dir
         # Isolate the range of values in present_values list
         field_values = []
-        rows = arcpy.SearchCursor(infc_directory,fields = "Dist_ID")
+        rows = arcpy.SearchCursor(infc_dir, fields="Dist_ID")
         for row in rows:
             instance = row.getValue("Dist_ID")
             field_values.append(instance)
@@ -182,7 +182,9 @@ class Split_Layer_Tool(object):
         # Select based off attribute value and make a new feature class that is stored in Results folder.
         for i in present_values:
             arcpy.management.SelectLayerByAttribute('lyr', "NEW_SELECTION", '"Dist_ID" = ' + str(i) + '' )
-            arcpy.management.CopyFeatures("lyr", os.path.join(os.path.split(infc_directory)[0],"district_" + str(i)))
+            arcpy.management.CopyFeatures("lyr", os.path.join(
+                                          os.path.split(infc_dir)[0],
+                                          "district_" + str(i)))
 
         return True
 
@@ -201,35 +203,35 @@ class Build_Districts_Tool(object):
         """Define parameter definitions"""
         # Input Feature Class
         param0 = arcpy.Parameter(
-            displayName = "Input Feature Class",
-            name = "in_feature",
-            datatype = "DEFeatureClass",
-            parameterType = "Required",
-            direction = "Input")
+            displayName="Input Feature Class",
+            name="in_feature",
+            datatype="DEFeatureClass",
+            parameterType="Required",
+            direction="Input")
 
         # only accept Polygon Feature Class
         param0.filter.list = ["Polygon"]
 
         # Name of Field to be Added
         param1 = arcpy.Parameter(
-            displayName = "Field Name",
-            name = "field_name",
-            datatype = "Field",
-            parameterType = "Required",
-            direction = "Input")
+            displayName="Field Name",
+            name="field_name",
+            datatype="Field",
+            parameterType="Required",
+            direction="Input")
         param1.parameterDependencies = [param0.name]
         param1.filter.list = ["LONG", "SHORT"]
 
         # Outpath
         param2 = arcpy.Parameter(
-            displayName = "Output Folder",
-            name = "field_value",
-            datatype = "GPFeatureLayer",
-            parameterType = "Required",
-            direction = "Output")
+            displayName="Output Folder",
+            name="field_value",
+            datatype="GPFeatureLayer",
+            parameterType="Required",
+            direction="Output")
 
         # list of parameters for tool
-        parameters = [param0,param1,param2]
+        parameters = [param0, param1, param2]
         return parameters
 
     def isLicensed(self):
@@ -243,11 +245,10 @@ class Build_Districts_Tool(object):
 
         n = numDistricts(infc)
 
-
         # Create Layer from id (default layer name is lyr)
         arcpy.management.MakeFeatureLayer(infc, "lyr")
         # Dissolve layer
-        arcpy.management.Dissolve(infc,outfc,"Dist_ID")
+        arcpy.management.Dissolve(infc, outfc, "Dist_ID")
         # add dissolveed layer to district_layers array
 
         # join all members of district_layers array
